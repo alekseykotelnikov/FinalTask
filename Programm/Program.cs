345b10ce923bfd0cb1abb76fd72fc810f101a7ca
aleksey <aleksey7008795@yandex.ru>
2022-03-19 18:16:44 +0300

    Add GetNewArray to sort string array and resize new array

--- Programm/Program.cs
+++ Programm/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк 
+// формирует массив из строк, длинна которых меньше либо равна 3 символа.
+Console.Clear();
+string[] GetNewArray(string[] firstArray)
+{
+    string[] newArray = new string[firstArray.Length];
+    int realSize = 0;
+    for (var i = 0; i < firstArray.Length; i++)
+    {
+        if (firstArray[i].Length <= 3)
+        {
+            newArray[realSize] = firstArray[i];
+            realSize++;
+        }
+    }
+    Array.Resize(ref newArray, realSize);
+    return newArray;
+}
